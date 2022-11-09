ce4e184513bf6db924d324700bde84053b5e6d8e
Meryem M'hamdi <meryemmhamdi1@gmail.com>
2022-11-09 12:24:34 -0800

    Added scripts for the evaluatio of Translate-Train with different languages

---
+++ multi_meta_ssd/scripts/symsearch/ft_translate_train_ar.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+MODE_TRANSFER=${1:-"MONO_MONO"}
+MODEL_TYPE=${2:-"sbert-retrieval"}
+MODEL_NAME_OR_PATH=${3:-"sentence-transformers/paraphrase-multilingual-mpnet-base-v2"}
+GPU_NUM=${4:-1}
+
+
+multi_meta_ssd symsearch_train --model_type $MODEL_TYPE \
+                          --model_name_or_path $MODEL_NAME_OR_PATH \
+                          --mode_transfer $MODE_TRANSFER \
+                          --gpu_order $GPU_NUM \
+                          --translate_train \
+                          --translate_train_langs "ar"
